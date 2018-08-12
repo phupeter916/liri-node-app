bdea6260d4c117e0795356e1a713a580acf0f70f
email <phupeter916@gmail.com>
2018-08-12 14:26:00 -0700

    current

--- liri.js
+++ liri.js
@@ -1,18 +1,23 @@
 
-var keys = require("./keys.js");
+
 require("dotenv").config();
-var request = require("request");
+var keys = require("./keys.js");
+
+// npm apps
 var Spotify = require('node-spotify-api');
-var fs = require('fs');
+var request = require("request");
+var Twitter = require('twitter');
+var fs = require("fs");
 
-//Add the code required to import the keys.js file and store it in a variable.
 
-//var spotify = new Spotify(keys.spotify);
+// access keys.js to get TWITTER and SPOTIFY api access
+//var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
 //var client = new Twitter(keys.twitter);
 
 var command = process.argv[2];
 var name = process.argv[3];
-
+ 
 /*
 Make it so liri.js can take in one of the following commands:
 * `my-tweets`
@@ -26,11 +31,22 @@ if else or swich statement
 if(command == "movie-this") {
     console.log(command);
     moviethis(name);
+    
 }
 
 if(command == "spotify-this-song") {
     console.log(command);
     spotifythissong(name);
+}
+
+if(command == "do-what-it-says") {
+    console.log(command);
+    dowhatitsays();
+}
+
+if(command == "my-tweets") {
+    console.log(command);
+    mytweets();
 }
 
 
@@ -45,23 +61,33 @@ if(command == "spotify-this-song") {
 function mytweets() {
     //This will show your last 20 tweets and when they were created at in your terminal/bash window.
 
-}
+    
+
+        
+        
+
+    };
 
 function spotifythissong(name) {
+    
     var spotify = new Spotify(keys.spotify);
-        if (!name){
-            name = 'The Weeknd';
-        }
-        spotify.search({ type: 'track', query: inputs }, function(err, data) {
+        
+        spotify.search({ 
+            type: 'track', 
+            query: name 
+        }, 
+            function(err, data) {
             if (err){
                 console.log('Error occurred: ' + err);
                 return;
             }
             var songInfo = data.tracks.items;
-            console.log("Artist(s): " + songInfo[0].artists[0].name);
-            console.log("Song Name: " + songInfo[0].name);
-            console.log("Preview Link: " + songInfo[0].preview_url);
-            console.log("Album: " + songInfo[0].album.name);
+            var logSpotify =
+                "\nArtist: " + songInfo[0].artists[0].name +
+                "\nSong title: " + songInfo[0].name +
+                "\nAlbum name: " + songInfo[0].album.name +
+                "\nURL Preview: " + songInfo[0].preview_url +
+            console.log(logSpotify)
     });
 }//end of function
   
@@ -99,9 +125,12 @@ function moviethis (movie_name) {
 
   // If the request is successful (i.e. if the response status code is 200)
   if (!error && response.statusCode === 200) {
+        
 
     // Parse the body of the site and recover just the imdbRating
     // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+
+    
     console.log("The movie's rating is: " + movieParse(response.body).imdbRating);
     console.log("The movie's title is: " + movieParse(response.body).Title);
     console.log("The movie's year is: " + movieParse(response.body).Year);
@@ -114,6 +143,17 @@ function moviethis (movie_name) {
   
   }
 });//end of request function
+
+function movie() {
+    if (name === undefined) {
+        title = "Mr.+Nobody";
+        moviethis();
+    } else if (title !== undefined) {
+        titleSplit = title.split(" ");
+        title = titleSplit.join("+");
+        moviethis();
+    };
+};
 
     // You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, the OMDB API requires an API key. You may use trilogy.
     /*
@@ -137,11 +177,28 @@ function moviethis (movie_name) {
 function dowhatitsays () {
     
     //Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.//
-    var fs = require("fs");
+    fs.readFile('random.txt', "utf8", function(error, data){
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(",");
+        if (dataArr[0] === "spotify-this-song") {
+            var check = dataArr[1].slice(1, -1);
+            spotify(check);
+        } else if (dataArr[0] === "my-tweets") {
+            var tweetname = dataArr[1].slice(1, -1);
+            twitter(tweetname);
+        } else if(dataArr[0] === "movie-this") {
+            var movie_name = dataArr[1].slice(1, -1);
+            movie(movie_name);
+        } 
+        
+    });
+}
 
 
     //It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.//
     //Feel free to change the text in that document to test out the feature for other commands.//
     
-}
+
 
